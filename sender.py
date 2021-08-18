2486acee6bcdd0d6bcfc57d752597bdaba3da357
Watchara Pongsri <giffyp@hotmail.com>
2021-08-18 12:19:28 +0700

    refactor code

--- sender.py
+++ sender.py
@@ -13,7 +13,7 @@ userid = configur.get('appsettings', 'RABBITMQ_USER')
 password = configur.get('appsettings', 'RABBITMQ_PASS')
 port = configur.getint('appsettings', 'RABBITMQ_PORT')
 virtual_host = configur.get('appsettings', 'RABBITMQ_VIRTUALHOST')
-client_key_pass = configur.get('appsettings', 'CLIENT_KEY_PASS')
+tls_key_pass = configur.get('appsettings', 'TLS_KEY_PASS')
 
 
 CurrentPath = os.path.dirname(os.path.abspath(__file__))
@@ -21,7 +21,7 @@ CurrentPath = os.path.dirname(os.path.abspath(__file__))
 context = ssl.create_default_context(cafile=os.path.join(
     CurrentPath, 'certs/server/ca_certificate.pem'))
 context.load_cert_chain(os.path.join(CurrentPath, "certs/client/client_certificate.pem"),
-                        os.path.join(CurrentPath, "certs/client/client_key.pem"), client_key_pass)
+                        os.path.join(CurrentPath, "certs/client/client_key.pem"), tls_key_pass)
 
 
 #context.verify_mode = ssl.CERT_NONE
